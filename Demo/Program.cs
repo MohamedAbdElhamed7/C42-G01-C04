361188e2edca0594de5754ec249c9a209d377453
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-24 01:55:09 +0300

    Loop pt1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Video 1 (Loop pt1)
+            //// 10 steps [Better For Performance]
+            //Console.WriteLine("1 T");
+            //Console.WriteLine("2 T");
+            //Console.WriteLine("3 T");
+            //Console.WriteLine("4 T");
+            //Console.WriteLine("5 T");
+            //Console.WriteLine("6 T");
+            //Console.WriteLine("7 T");
+            //Console.WriteLine("8 T");
+            //Console.WriteLine("9 T");
+            //Console.WriteLine("10 T");
+
+            ////32 Steps [Better For Maintenance ]
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{i} T");    
+            //}
+
+            #endregion
         }
     }
 }
