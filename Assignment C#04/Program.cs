830e55def18167b9510277b499f399948ab0c6cf
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-24 06:50:51 +0300

    Q1 solved

---
+++ Assignment C#04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_C_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            //1- Write a program that allows the user to insert an integer
+            //then print all numbers between 1 to that number.
+
+            //int Number;
+            //bool Flag;
+            //do
+            //{
+            //    Console.WriteLine("Please enter an a number");
+            //    Flag = int.TryParse(Console.ReadLine(), out Number);
+            //    for (int i = 1; i <= Number; i++)
+            //    {
+            //        Console.Write($"{i}");
+            //        if (i != Number)
+            //            Console.Write(", ");
+
+            //    }
+            //} while (!Flag);  
+
+            #endregion
+
+
+        }
+    }
+}
