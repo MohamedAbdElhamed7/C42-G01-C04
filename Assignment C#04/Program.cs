61311eb687d15c9dcd51664b1f2f5523daf229da
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-24 07:13:06 +0300

    Q16 Solved

--- Assignment C#04/Program.cs
+++ Assignment C#04/Program.cs
@@ -297,6 +297,25 @@
             //Console.WriteLine($"maximum nu = {max}");
             //Console.WriteLine($"maximum nu = {min}");
             #endregion
+
+            #region Q16
+            //15- Write a program in C# Sharp to find the maximum and minimum element in an array
+            //int[] arr = [0, 1, 2, 1, 3, 4, 3, 2, 0];
+            //int max = int.MaxValue;
+            //int min= int.MinValue;  
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i]> max)
+            //        max = arr[i]; 
+
+            //    if (arr[i]> min)
+            //        min = arr[i];   
+            //}
+
+            //Console.WriteLine($"maximum nu = {max}");
+            //Console.WriteLine($"maximum nu = {min}");
+            #endregion
         }
     }
 }
